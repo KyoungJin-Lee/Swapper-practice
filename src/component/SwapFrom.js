4a2c3a2b4bef1b7c6b4c7686034f2c9507b7ae9d
Tony Lee <2_kj@daum.net>
2022-04-07 15:52:47 +0900

    icon_add

--- src/component/SwapFrom.js
+++ src/component/SwapFrom.js
@@ -10,8 +10,8 @@ function SwapFrom() {
                 <select>
                     <option selected value="ton">TON</option>
                     <option value="wton">WTON</option>
-                    <option value="AUR">AUR</option>
-                    <option value="DOC">DOC</option>
+                    <option value="aur">AUR</option>
+                    <option value="doc">DOC</option>
                 </select>
             </p>
             <p className="amount">
